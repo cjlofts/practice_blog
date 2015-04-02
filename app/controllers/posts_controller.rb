a7a29d220b4814bab0f625747f730722ac145501
Ciaran Lofts <cjlofts@sky.com>
2015-04-01 18:55:06 -0700

    Basic CRUD added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,52 @@
 class PostsController < ApplicationController
+
+  before_action :find_post , only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(require_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(require_params)
+      redirect_to @post , notice: "Post Successfully Updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find params[:id]
+  end
+
+  def require_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  
+
+  root "posts#index"
+
+  # get "/posts/new" => "posts#new"
+  # post "/posts/new" => "posts#create", as: :posts
+
+  # get "/posts/:id" => "posts#show", as: :post
+  # patch "/posts/:id" => "posts#update"
+  # delete "/posts/:id" => "posts#destroy"
+
+  # get "/posts/edit/:id" => "posts#edit" , as: :edit_post
+
+  resources :posts do
+  end
+
 end
